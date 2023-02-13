a0bb399377dfabbaced582c28997a7523d0d8776
eduardocarlostoledo <113210953+eduardocarlostoledo@users.noreply.github.com>
2023-02-13 18:45:04 -0300

    getProducts and getProductsId, routes and controlles

--- api/src/controllers/productController.js
+++ api/src/controllers/productController.js
@@ -1,26 +1,47 @@
-const {Product, User, Type, Trademark} = require('../db')
+const { Product, User, Type, Trademark } = require("../db");
+
+const getProducts = async () => {
+    try {
+      const allProducts = await Product.findAll();
+      return allProducts;
+    } catch (error) {
+      throw new Error("Error retrieving products: " + error.message);
+    }
+  };
+  
+
+const getProductId = async (productId) => {
+  try {
+    const result = await Product.findByPk(productId);
+    if (result) return result;
+    throw new Error("Product not found with ID: " + productId);
+  } catch (error) {
+    throw new Error("Error retrieving product: " + error.message);
+  }
+};
 
 const postProduct = async (product) => {
-    console.log(product)
-    const {name, price, type, trademark} = product
-    if(!name || !price) throw Error('Mandatory data missing')
-    else {
-        try {
-            const newProduct = await Product.create({
-                name,price,
-                trademarkId: type,
-                typeId: trademark
-            })
-          
-            return newProduct
-        } catch (error) {
-            throw Error(error.message)
-        }
+  console.log(product);
+  const { name, price, type, trademark } = product;
+  if (!name || !price) throw Error("Mandatory data missing");
+  else {
+    try {
+      const newProduct = await Product.create({
+        name,
+        price,
+        trademarkId: type,
+        typeId: trademark,
+      });
+
+      return newProduct;
+    } catch (error) {
+      throw Error(error.message);
     }
-}
-
-
+  }
+};
 
 module.exports = {
-    postProduct
-}
+  postProduct,
+  getProducts,
+  getProductId,
+};
--- api/src/routes/productRouter.js
+++ api/src/routes/productRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const {postProduct} = require('../controllers/productController')
+const {postProduct, getProducts, getProductId} = require('../controllers/productController')
 
 const productRouter = Router()
 
@@ -11,5 +11,28 @@ productRouter.post('/', async (req,res) => {
         res.status(400).json(error.message)
     }
 })
+productRouter.get("/products", async (req, res) => {
+    try {
+      const allProducts = await getProducts();
+      res.status(200).json({ data: allProducts, message: "Listado de productos" });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+  
+  productRouter.get("/products/:id", async (req, res) => {
+    const productId = req.params.id;
+    try {
+      const result = await getProductId(productId);
+      if (result) {
+        res.status(200).json({ data: result, message: "Producto solicitado" });
+      } else {
+        res.status(404).json({ error: "Producto no encontrado" });
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+  
 
 module.exports = {productRouter}
