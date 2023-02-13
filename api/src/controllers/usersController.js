18350da8bcc85ee48dba0b9383971b3ae2d3acae
eduardocarlostoledo <113210953+eduardocarlostoledo@users.noreply.github.com>
2023-02-13 16:42:44 -0300

    userPut route and controller

---
+++ api/src/controllers/usersController.js
@@ -0,0 +1,21 @@
+const {User} = require('../db')
+
+
+const putUser = async (user) => {
+    const { name, lastname, email, image, password, phonenumber, country, city, address } = user
+
+if (!name || !lastname || !email || !image || !password || !phonenumber || !country || !city || !address)  throw Error('User data missing')
+else {
+    try {
+        const changeUser = await User.update({ name, lastname, email, image, password, phonenumber, country, city, address })
+        return changeUser
+    } catch (error) {
+        throw Error(error.message)
+    }
+}
+
+}
+
+module.exports = {
+    putUser
+}
