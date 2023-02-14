d4d4ed9796a5faa5af2cfe8117484f8dccd217bf
Lucia Daniela Radwanski <luciadanielaradwanski@gmail.com>
2023-02-14 14:57:26 -0300

    ProductActions

--- client/src/redux/ProductActions.js
+++ client/src/redux/ProductActions.js
@@ -3,4 +3,12 @@ import {getAllProducts, getProductsById} from './ProductSlice';
 
 export const getProducts = () => (dispatch) => {
     axios('http://localhost:3001/products')
+    .then((r) => dispatch(getAllProducts(r.data.results)))
+    .catch((e) => console.log(e))
 }
+export const getProdById = () => (dispatch) => {
+    axios('http://localhost:3001/products')
+    .then((r) => dispatch(getProductsById(r.data)))
+    .catch((e) => console.log(e))
+}
+
