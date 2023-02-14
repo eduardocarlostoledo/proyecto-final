d6841dca7d350e75a64712963d68aa7f206b0647
LeanKrn <leandrotomask@gmail.com>
2023-02-14 14:28:06 -0300

    update Product

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,12 +1,24 @@
 import "./Products.css";
+import Json from "./Productos_PF.json"
 
 export const Products = () => {
   return (
     <div className="DivProducts">
       <h1>Products</h1>
       <div className="DivCardsFilters">
+        <div className="DivFilter">
         <h2>Filters</h2>
-        <h1>Cards</h1>
+
+        </div>
+        <div className="DivAllCards">
+          { Json.map( (prod) => (
+            <div className="DivCard">
+              <img src={prod.image}></img>
+              <h2>{prod.name}</h2>
+              <h3>{prod.price}</h3>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
