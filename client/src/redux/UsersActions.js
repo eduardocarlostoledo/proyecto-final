2b0e7ebf6d60d9f413ceb96552ead4448ee9ea4a
Lucia Daniela Radwanski <luciadanielaradwanski@gmail.com>
2023-02-14 19:00:10 -0300

    usersActions

--- client/src/redux/UsersActions.js
+++ client/src/redux/UsersActions.js
@@ -4,17 +4,17 @@ import axios from 'axios';
 import {getUsers, getUsersId, putUser} from './UsersSlice';
 
 export const getUser = () => (dispatch) => {
-    axios('http://localhost:3001/products')
+    axios('http://localhost:3001/users')
     .then((r) => dispatch(getUsers(r.data.results)))
     .catch((e) => console.log(e))
 }
 export const getUsersById = (id) => (dispatch) => {
-    axios(`http://localhost:3001/products/${id}`)
+    axios(`http://localhost:3001/users/${id}`)
     .then((r) => dispatch(getUsersId(r.data)))
     .catch((e) => console.log(e))
 }
 export const putByUser = () => (dispatch) => {
-    axios('http://localhost:3001/')
+    axios('http://localhost:3001/register')
     .then((r) => dispatch(putUser(r.data)))
     .catch((e) => console.log(e))
 }
