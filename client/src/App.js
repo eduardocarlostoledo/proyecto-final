c77aa961f5ae125a22357fd095b74ff8e3a8ae4a
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-02-15 12:10:42 -0300

    hola

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,18 @@ import { Products } from "./components/Products";
 import { Detail } from "./components/Detail";
 import { BuildPc } from "./components/BuildPc";
 import { NavBar } from "./components/NavBar";
-import { Register } from "./components/Register";
+import { Register}  from "./components/Register";
 import { Profile } from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
+      <NavBar />
       <Route exact path="/" component={Home} />
-
       <Route path="/Products" component={Products} />
-
       <Route path="/Detail" component={Detail} />
-
       <Route path="/BuildPc" component={BuildPc} />
-
       <Route path="/Register" component={Register} />
-
       <Route path="/Profile" component={Profile} />
     </div>
   );
