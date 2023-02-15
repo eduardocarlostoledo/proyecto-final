0e3b548899c527118ec39f4346ce60ba90772194
LeanKrn <leandrotomask@gmail.com>
2023-02-15 10:31:55 -0300

    Update Product css and component

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,10 +1,17 @@
+import "../styles/Products.css";
+import Card from './Card'
 
 export const Products = () => {
-    
-    return(
-        <div>
-            <h1>Products</h1>
-            
+  return (
+    <div className="DivProducts">
+      <h1>Products</h1>
+      <div className="DivCardsFilters">
+        <div className="DivFilter">
+        <h2>Filters</h2>
+
         </div>
-    )
-}
+        <Card></Card>
+      </div>
+    </div>
+  );
+};
