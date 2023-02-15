388c12d94c05f6f19ad1f03368e63798d4e93914
LeanKrn <leandrotomask@gmail.com>
2023-02-15 17:56:07 -0300

    add images and update carrusel

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import Card from './Card'
 import '../styles/Home.css';
-import laptop from '../images/laptop.jpg'
+import asus from '../images/asusMonitor.jpg'
 import Ryzen from '../images/Ryzen7mil.jpg'
 import samsung from '../images/samsungMonitores.jpg'
 
@@ -15,13 +15,13 @@ export const Home = () => {
                 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                        <img src={laptop} class="d-block w-100" alt="" />
+                        <img src={asus} class="d-block w-80" alt="" />
                         </div>
                         <div class="carousel-item">
-                        <img src={Ryzen} class="d-block w-100" alt="" />
+                        <img src={Ryzen} class="d-block w-80" alt="" />
                         </div>
                         <div class="carousel-item">
-                        <img src={samsung} class="d-block w-100" alt="" />
+                        <img src={samsung} class="d-block w-80" alt="" />
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
