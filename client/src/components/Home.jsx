5700259400c332e0016a685f0fed19bb5a5812fb
Lucia Daniela Radwanski <luciadanielaradwanski@gmail.com>
2023-02-15 12:20:45 -0300

    'Home/css'

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,7 +6,9 @@ export const Home = () => {
     return(
         <div className="HomeContainer">
             <h1>Home</h1>
-            <Card/>
+            <div className="CardContainer">
+                <Card/>
+            </div>
         </div>
     )
 }
