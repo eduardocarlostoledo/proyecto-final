af4ce396ac98e14aafd463270ff47275269a258a
Lucia Daniela Radwanski <luciadanielaradwanski@gmail.com>
2023-02-17 10:58:06 -0300

    commit

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -11,7 +11,7 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use('/products', productRouter)
-router.use('/register', userRouter)
+router.use('/users', userRouter)
 
 
 
