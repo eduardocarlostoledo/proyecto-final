df6871bee5728ef1b22490bf2306891f539e705a
Franco Agustín <114515778+FrancooChaparro@users.noreply.github.com>
2023-02-18 17:30:30 -0300

    navsearch

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,32 +6,15 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 export const NavBar = () => {
     return (
-        // <div className='NavDiv'>
-        //     <Search/>
-        //     <div className='NavDiv2'>
-
-        //         <Link to="/" className="NavLink">home</Link>
-
-        //         <Link to="/Products" className="NavLink">products</Link>
-
-        //         <Link to="/BuildPc" className="NavLink">build pc</Link>
-
-        //         <Link to="/Register" className="NavLink">Register</Link>
-
-        //         <Link to="/Profile" className="NavLink">Profile</Link>
-        //     </div>
-        // </div>
         <div className='NavDiv'>
         <div className="BuildAndProducts">
         <Link to="/"><button><ion-icon name="home-outline"></ion-icon> Home </button></Link>
         <Link to="/Products"><button>Products</button></Link>
         <Link to="/BuildPc"><button>Build PC</button></Link>
             </div>
-
         <div className="ContainerSearch">
             <Search />
         </div>
-
         <div className="ContainerInfo">
             <div className="LogAndSign">
             <Link to="/Login"><button className="BtnUser" >Log in</button></Link>
