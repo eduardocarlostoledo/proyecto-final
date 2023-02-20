2faea12a483990449a83d03656a52dc37702f30f
laiamia5 <69235228+L03A95@users.noreply.github.com>
2023-02-20 16:39:36 -0300

    asd

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const {productRouter} = require('./productRouter')
 const {userRouter} = require("./userRouter.js")
 const {cartRouter}=require("./cartRouter.js")
+const {filterRouter} = require("./filterRouter")
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -13,7 +14,7 @@ const router = Router();
 router.use('/products', productRouter)
 router.use('/users', userRouter)
 router.use('/cart', cartRouter)
-
+router.use('/filter',filterRouter)
 
 // router.use('/users', userRouter)
 
