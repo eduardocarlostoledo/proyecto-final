deaf3ff22c8228d6f03051fdc5f43439b6c713eb
eduardo carlos toledo <113210953+eduardocarlostoledo@users.noreply.github.com>
2023-03-07 22:00:19 -0300

    fixes de rebase -.-

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -27,6 +27,7 @@ export default function Cart() {
     const total = cartItems
     .reduce((acc, item) => acc + item.price * item.amount, 0)
     .toFixed(1);
+
 //seteamos preferencia
   const preferencia = cartItems.map((item) => ({
     product_description: item.name,
@@ -44,46 +45,49 @@ export default function Cart() {
     buyer_email,
   });
 //seteamos preferencia
-
   const orderData = {
     quantity: 1,
     description: description.toString(),
     price: total,
   };
-
-    const handleCheckout = (e) => {
-      e.preventDefault();
-      if (!cartItems.length) swal("Cart is empty", "Cart is empty", "error");
-      else if (userActive === null) {
-        swal("You must log in to buy!", "You must log in to buy!", "error");
-        navigate("/login");
-      } else {
-        fetch("http://localhost:3001/pay/preference", {
+    
+ const handleCheckout = (e) =>{
+        e.preventDefault();
+        if(!cartItems.length) swal('Cart is empty', "Cart is empty", 'error')
+        else if(userActive===null){
+            swal('You must log in to buy!', "You must log in to buy!", 'error')
+            navigate("/login")
+        }
+        else{ 
+          fetch("http://localhost:3001/pay/preference", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(preferencia),
         });
-        fetch("http://localhost:3001/pay/create_preference", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(orderData),
-        })
-          .then(function (response) {
-            console.log("RESPONSE", response);
-            return response.json();
-          })
-          .then(function (preference) {
-            createCheckoutButton(preference.id);
-          })
-          .catch(function () {
-            alert("Unexpected error");
-          });
-      }
-    };
+            fetch("http://localhost:3001/pay/create_preference", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(orderData),
+            })
+            .then(function (response) {
+                console.log("RESPONSE" , response)
+                return response.json();
+            })
+
+            .then(
+                function (preference) {
+                createCheckoutButton(preference.id);
+            })
+            .catch(function () {
+                alert("Unexpected error");
+            })
+        }
+    }
+
 
     // Create preference when click on checkout button
     const createCheckoutButton = (preferenceId) => {
@@ -115,6 +119,7 @@ export default function Cart() {
         }
     }
 
+
     return (
         <div className='ContainerCart'>
             <h2 className='h2'>Shopping Cart</h2>
@@ -123,33 +128,33 @@ export default function Cart() {
                     {cartItems.length == 0 ? (
                         <p>Cart is empty</p>
 
-                  ) : ( cartItems.map(item => (
-                      <div >
-                          <ItemCart
-                              name= {item.name}
-                              price= {item.price}
-                              amount= {item?.amount}
-                              image= {item.image}
-                              prodId= {item.prodId}
-                              key={item.id}
+                    ) : ( cartItems.map(item => (
+                        <div >
+                            <ItemCart
+                                name= {item.name}
+                                price= {item.price}
+                                amount= {item?.amount}
+                                image= {item.image}
+                                prodId= {item.prodId}
+                                key={item.id}
 
-                              handleDeleteAllCart={handleDeleteAllCart}
-                          /> 
+                                handleDeleteAllCart={handleDeleteAllCart}
+                            /> 
 
-                      </div>
+                        </div>
 
-                      ))
-                  )}
+                        ))
+                    )}
 
-      
-          </div>
-          <button className='ButtonDeleteAll' onClick={handleDeleteAllCart}>Delete All Cart</button>
+        
+            </div>
+            <button className='ButtonDeleteAll' onClick={handleDeleteAllCart}>Delete All Cart</button>
 
-          <h3 className='h3'>Total: ${total}</h3>
-          <div className='BotonCheckout'>
-              <button className='ButtonCart' onClick={handleCheckout}>Checkout</button>
-              <div  id="button-checkout"></div>
-          </div>
-      </div>
-  );
+            <h3 className='h3'>Total: ${total}</h3>
+            <div className='BotonCheckout'>
+                <button className='ButtonCart' onClick={handleCheckout}>Checkout</button>
+                <div  id="button-checkout"></div>
+            </div>
+        </div>
+    );
 }
