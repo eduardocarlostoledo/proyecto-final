95a821c3eb27e063a1fed97cb7cdc86ba3283a90
Lucia Daniela Radwanski <luciadanielaradwanski@gmail.com>
2023-03-08 17:50:28 -0300

    responsive de home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,7 @@ import '../styles/Home.css';
 import asus from '../images/asusMonitor.jpg'
 import Ryzen from '../images/Ryzen7mil.jpg'
 import samsung from '../images/samsungMonitores.jpg'
+import { Link } from 'react-router-dom';
 
 
 /** Coloque un diseño de Boostrap para las Imagenes */
@@ -33,101 +34,70 @@ export const Home = () => {
                     </button>
                 </div>
             </div> 
-            <section className="post" id="post">
-                
-                <div className="contentBx">
-                    <div className="postColumn">
-                        <div className="postBox extraHeight">
-                            <div className="img">
-                                <img src="https://www.venex.com.ar/products_images/1625487702_0413607bi.jpg" alt="" className='cover'/>
-                            </div>
-                            <div className="textBox">
-                                <h3>Auriculares gamer inalámbricos Corsair Virtuoso RGB Wireless SE espresso con luz rgb LED</h3>
-                                <a href="/products" className="btnHome">Read more</a>
-                            </div>
+            <div className='ContainerGalery'>
+                <div className='Galery'>
+                    <div className='Card'>
+                        <div className='Image'>
+                            <img className='CardImg' src="https://www.venex.com.ar/products_images/1625487702_0413607bi.jpg" alt="Imagen 1" />
                         </div>
-                        <div className="postBox">
-                            <div className="img">
-                                <img src="https://http2.mlstatic.com/D_NQ_NP_2X_912771-MLA48049398762_102021-F.webp" alt="" className='cover'/>
-                            </div>
-                            <div className="textBox">
-                                <h3>Memoria RAM Vengeance RGB Pro gamer color negro 16GB 2 Corsair CMW16GX4M2C3200C16</h3>
-                                <a href="/products" className="btnHome">Read more</a>
-                            </div>
-                        </div>
-                        <div className="postBox">
-                            <div className="img">
-                                <img src="https://http2.mlstatic.com/D_NQ_NP_2X_601095-MLA53751174902_022023-F.webp" alt="" className='cover'/>
-                            </div>
-                            <div className="textBox">
-                                <h3>Funda Libro Para Tablet Samsung A7 Lite T220 Cover</h3>
-                                <a href="/products" className="btnHome">Read more</a>
-                            </div>
+                        <div>
+                            <p className='CardText'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dicta cupiditate deleniti quidem eligendi velit non mollitia a impedit ad.</p>
+                            <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
-                    <div className="postColumn ">
-                        <div className="postBox">
-                            <div className="img">
-                                <img src="https://http2.mlstatic.com/D_NQ_NP_2X_796880-MLA43468491094_092020-F.webp" alt="" className='cover'/>
-                            </div>
-                            <div className="textBox">
-                                <h3>Cooler Cpu Id-cooling Se-902-sd Intel 1200 115x Y Amd Am4</h3>
-                                <a href="/products" className="btnHome">Read more</a>
-                            </div>
-                        </div>
-                        <div className="postBox extraHeight">
-                            <div className="img">
-                                <img src="https://http2.mlstatic.com/D_NQ_NP_2X_657637-MLA51822045232_102022-F.webp" alt="" className='cover'/>
-                            </div>
-                            <div className="textBox">
-                                <h3>Auriculares Gamer Redragon H220 Themis Negro Con Luz Led Roja</h3>
-                                <a href="/products" className="btnHome">Read more</a>
-                            </div>
-                        </div>
-                        <div className="postBox">
-                            <div className="img">
-                                <img src="https://http2.mlstatic.com/D_NQ_NP_2X_884180-MLA42143887390_062020-F.webp" alt="" className='cover'/>
-                            </div>
-                            <div className="textBox">
-                                <h3>Notebook Acer Aspire 3 Dual Core 8gb/ssd 240 L14.01 W10</h3>
-                                <a href="/products" className="btnHome">Read more</a>
-                            </div>
+                    <div className='Card'>
+                        <img className='CardImg' src="https://www.deffo.com.ar/wp-content/uploads/2022/07/GV-N308TVISION-OC-12GD-1.webp" alt="Imagen 1" />
+                        <div>
+                            <p className='CardText'>Multiprocesadores integrados con interfaz de memoria, sistema de refrigeración, ventiladores giratorios alternos Placa trasera metálica de protección</p>
+                            <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
-                    <div className="postColumn">
-                        <div className="postBox">
-                            <div className="img">
-                                <img src="https://http2.mlstatic.com/D_NQ_NP_2X_676450-MLA46168520744_052021-F.webp" alt="" className='cover'/>
-                            </div>
-                            <div className="textBox">
-                                <h3>Notebook Asus Vivo Ideal Diseño Ssd 256 Teclado Ilum Win 10</h3>
-                                <a href="/products" className="btnHome">Read more</a>
-                            </div>
-                        </div>
-                        <div className="postBox">
-                            <div className="img">
-                                <img src="https://http2.mlstatic.com/D_NQ_NP_2X_711390-MLA52684067237_122022-F.webp" alt="" className='cover'/>
-                            </div>
-                            <div className="textBox">
-                                <h3>Placa de video AMD PowerColor  Red Devil Radeon RX 6700 Series RX 6700 XT AXRX 6700XT 12GBD6-3DHE/OC 12GB</h3>
-                                <a href="/products" className="btnHome">Read more</a>
-                            </div>
-                        </div>
-                        <div className="postBox extraHeight">
-                            <div className="img">
-                                <img src="https://http2.mlstatic.com/D_NQ_NP_2X_627990-MLA41919075615_052020-F.webp" alt="" className='cover'/>
-                            </div>
-                            <div className="textBox">
-                                <h3>Mother Gigabyte Amd Ga-a320m-h Am4 Gamer Ddr4</h3>
-                                <a href="/products" className="btnHome">Read more</a>
-                            </div>
+                    <div className='Card'>
+                        <img className='CardImg' src="https://http2.mlstatic.com/D_NQ_NP_662444-MLA41254447964_032020-O.webp" alt="Imagen 1" />
+                        <div>
+                            <p className='CardText'>Auriculares gamer inalámbricos Corsair Virtuoso RGB Wireless SE espresso con luz rgb LED.</p>
+                            <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
+                    <div className='Card'>
+                        <img className='CardImg' src="https://gorilagames.com/img/Public/1019-producto-corsari-blanca-8914.jpg" alt="Imagen 1" />
+                        <div>
+                            <p className='CardText'>Memoria RAM Vengeance RGB Pro gamer color negro 16GB 2 Corsair CMW16GX4M2C3200C16</p>
+                            <button class='CardButton'><Link to='/Products'>View More</Link></button>
+                        </div>
+                    </div>
+                    <div className='Card'>
+                        <img className='CardImg' src="https://http2.mlstatic.com/D_NQ_NP_2X_796880-MLA43468491094_092020-F.webp" alt="Imagen 1" />
+                        <div>
+                            <p className='CardText'>Cooler Cpu Id-cooling Se-902-sd Intel 1200 115x Y Amd Am4</p>
+                            <button class='CardButton'><Link to='/Products'>View More</Link></button>
+                        </div>
+                    </div>
+                    <div className='Card'>
+                        <img className='CardImg' src="https://http2.mlstatic.com/D_NQ_NP_2X_825550-MLA51822045233_102022-F.webp" alt="Imagen 1" />
+                        <div>
+                            <p className='CardText'>Auriculares Gamer Redragon H220 Themis Negro Con Luz Led Roja</p>
+                            <button class='CardButton'><Link to='/Products'>View More</Link></button>
+                        </div>
+                    </div>
+                    <div className='Card'>
+                        <img className='CardImg' src="https://computelweb.com/wp-content/uploads/2020/12/G903-FRENTE.png" alt="Imagen 1" />
+                        <div>
+                            <p className='CardText'>Tiene un sistema de detección de movimiento óptico te permitirá mover el cursor de una manera más precisa y sensible que en los sistemas tradicionales.</p>
+                            <button class='CardButton'><Link to='/Products'>View More</Link></button>
+                        </div>
+                    </div>
+                    <div className='Card'>
+                        <img className='CardImg' src="https://www.comeros.com.ar/wp-content/uploads/2022/01/Comeros-ASUS-ROG-STRIX-RTX3060TI-O8G-V2-GAMING-fcc87b-1.webp" alt="Imagen 1" />
+                        <div className='CardB'>
+                            <p className='CardText'>ASUS ROG Strix RTX 3060 Ti</p>
+                            <button class='CardButton'><Link to='/Products'>View More</Link></button>
+                        </div>
+                    </div>
+                    
                 </div>
-                <div className="title">
-                    <a href="#" className="btnHome mgt60">Load More</a>
-                </div>
-            </section>
+            </div>
+
         </div>
     )
 }
