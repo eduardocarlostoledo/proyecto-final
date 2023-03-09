018d3565e349116a823dd71c5e2e63ed3eda8459
Lucia Daniela Radwanski <luciadanielaradwanski@gmail.com>
2023-03-09 01:17:18 -0300

    detalles del home y de profile

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -41,56 +41,57 @@ export const Home = () => {
                             <img className='CardImg' src="https://www.venex.com.ar/products_images/1625487702_0413607bi.jpg" alt="Imagen 1" />
                         </div>
                         <div>
-                            <p className='CardText'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dicta cupiditate deleniti quidem eligendi velit non mollitia a impedit ad.</p>
+                            <p className='CardText'>The highest officially supported memory speed is 3200 MHz, but with overclocking (and the right memory modules) you can go even higher.</p>
                             <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
                     <div className='Card'>
                         <img className='CardImg' src="https://www.deffo.com.ar/wp-content/uploads/2022/07/GV-N308TVISION-OC-12GD-1.webp" alt="Imagen 1" />
                         <div>
-                            <p className='CardText'>Multiprocesadores integrados con interfaz de memoria, sistema de refrigeración, ventiladores giratorios alternos Placa trasera metálica de protección</p>
+                            <p className='CardText'>Multiprocessors with memory interface, cooling system,  and alternate rotating fans Protective metal back plate.</p>
                             <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
                     <div className='Card'>
                         <img className='CardImg' src="https://http2.mlstatic.com/D_NQ_NP_662444-MLA41254447964_032020-O.webp" alt="Imagen 1" />
                         <div>
-                            <p className='CardText'>Auriculares gamer inalámbricos Corsair Virtuoso RGB Wireless SE espresso con luz rgb LED.</p>
+                            <p className='CardText'>Corsair Virtuoso RGB Wireless SE espresso wireless gaming and headset with rgb LED light.</p>
                             <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
                     <div className='Card'>
                         <img className='CardImg' src="https://gorilagames.com/img/Public/1019-producto-corsari-blanca-8914.jpg" alt="Imagen 1" />
                         <div>
-                            <p className='CardText'>Memoria RAM Vengeance RGB Pro gamer color negro 16GB 2 Corsair CMW16GX4M2C3200C16</p>
+                            <p className='CardText'>Thanks to RGB lighting control Tec. (patent pending), your system can be synchronized with any motherboard platform for beautiful RGB lighting.</p>
                             <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
                     <div className='Card'>
                         <img className='CardImg' src="https://http2.mlstatic.com/D_NQ_NP_2X_796880-MLA43468491094_092020-F.webp" alt="Imagen 1" />
                         <div>
-                            <p className='CardText'>Cooler Cpu Id-cooling Se-902-sd Intel 1200 115x Y Amd Am4</p>
+                            <p className='CardText'>
+Master Edition Hyper 212 Black Heatsink, CPU Air Cooler, Silencio FP120 Fan, Anodized Black, Brushed Nickel Fins, 4 Direct Contact Copper.</p>
                             <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
                     <div className='Card'>
                         <img className='CardImg' src="https://http2.mlstatic.com/D_NQ_NP_2X_825550-MLA51822045233_102022-F.webp" alt="Imagen 1" />
                         <div>
-                            <p className='CardText'>Auriculares Gamer Redragon H220 Themis Negro Con Luz Led Roja</p>
+                            <p className='CardText'>Gamer Headphones Redragon H220 Themis Black With Red Led Light</p>
                             <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
                     <div className='Card'>
                         <img className='CardImg' src="https://computelweb.com/wp-content/uploads/2020/12/G903-FRENTE.png" alt="Imagen 1" />
                         <div>
-                            <p className='CardText'>Tiene un sistema de detección de movimiento óptico te permitirá mover el cursor de una manera más precisa y sensible que en los sistemas tradicionales.</p>
+                            <p className='CardText'>It has an optical movement detection system that will allow you to move the cursor in a more precise and sensitive way than in traditional systems.</p>
                             <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
                     <div className='Card'>
                         <img className='CardImg' src="https://www.comeros.com.ar/wp-content/uploads/2022/01/Comeros-ASUS-ROG-STRIX-RTX3060TI-O8G-V2-GAMING-fcc87b-1.webp" alt="Imagen 1" />
                         <div className='CardB'>
-                            <p className='CardText'>ASUS ROG Strix RTX 3060 Ti</p>
+                            <p className='CardText'>Notebook Asus Rog Strix G17 16gb Ryzen 7 6800h 512gb Pcreg</p>
                             <button class='CardButton'><Link to='/Products'>View More</Link></button>
                         </div>
                     </div>
